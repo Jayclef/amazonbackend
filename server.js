576647e3f5d0ef48bc30cabae64d814825e13c39
Mba James <43511109+Jayclef@users.noreply.github.com>
2022-06-16 22:16:50 +0100

    Update server.js

--- server.js
+++ server.js
@@ -33,14 +33,15 @@ app.get("/api/keys/paypal", (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || "sb");
 });
 
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "/frontend/build")));
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
-);
+
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
+
+app.get('/',(req,res) => {
+  res.send('APP IS RUNNING')
+})
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`server at http://localhost:${port}`);
